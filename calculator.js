ceaffe527837b8a0908917d37a24ca7c1751f3ab
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-12 16:36:41 +0000

    initial commit

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const sum = function(a, b){
+    return a + b;
+};
+
+const subtract = null;
+
+const multiply = null;
+
+const divide = null;
+
+const modulus = null;
+
+const even = null;
+
+const odd = null;
+
+module.exports = { 
+    sum, 
+    subtract, 
+    multiply, 
+    divide, 
+    modulus, 
+    even, 
+    odd 
+};
---
+++ calculator.test.js
@@ -0,0 +1,55 @@
+const {
+  sum,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  even,
+  odd,
+} = require("./calculator");
+
+describe('sum', () => { 
+  
+  test('can add two small positive numbers', () => {
+    const expected = 5;
+    const actual = sum(2, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add two large positive numbers', () => { 
+
+  });
+
+  test('can add two negative numbers', () => {
+    
+  });
+
+  test('can add zero', () => {
+    
+  });
+
+});
+
+describe('subtract', () => {
+
+});
+
+describe('multiply', () => {
+
+});
+
+describe('divide', () => {
+
+});
+
+describe('modulus', () => {
+
+});
+
+describe('even', () => {
+
+});
+
+describe('odd', () => {
+
+});
